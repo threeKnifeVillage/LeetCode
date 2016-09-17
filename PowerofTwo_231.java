43084637e76dcad0fbcd9f6834b8808abaf120f5
threeKnifeVillage <fancycup@126.com>
2016-09-17 09:09:28 +0800

    Add files via upload

---
+++ PowerofTwo_231.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if (n <= 0 || n > Integer.MAX_VALUE) {
+            return false;
+        }
+        
+        if (n == 1) {
+            return true;
+        }
+        
+        while (n%2 == 0) {
+            n=n/2;
+            
+            if (n==1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
