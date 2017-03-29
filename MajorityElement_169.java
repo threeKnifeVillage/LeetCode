998e7883c93eb6aca657adc81cfa8c219ec5b961
threeKnifeVillage <fancycup@126.com>
2017-03-29 19:49:02 +0800

    Add files via upload

---
+++ MajorityElement_169.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        long count = Math.round((nums.length / 2));
+        Map<Integer, Integer> majorityElementMap = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer appearCount = majorityElementMap.get(num);
+            if (appearCount == null) {
+                appearCount = 0;
+            }
+            
+            majorityElementMap.put(num, appearCount + 1);
+            
+            if (appearCount + 1 > count) {
+                return num;
+            }
+        }
+        
+        return 0;
+    }
+}
